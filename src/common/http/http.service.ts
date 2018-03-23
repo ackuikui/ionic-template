306bc1c209b1e194851f0ff73ff3e58e9a0c23ca
kevin <kevin.ji@yoov.com>
2018-03-23 15:51:59 +0800

    init

---
+++ src/common/http/http.service.ts
@@ -0,0 +1,165 @@
+import {Injectable} from "@angular/core";
+import {Http, Headers, RequestOptionsArgs} from "@angular/http";
+import {Observable} from "rxjs";
+import * as _ from "lodash";
+import {URLSearchParams} from "@angular/http";
+import {EnvironmentConfig} from "../config/config";
+
+export interface IMeta {
+    pagination?: {
+        count?: number,
+        currentPage?: number,
+        nextPage?: number,
+        prevPage?: number
+        totalCount?: number,
+    }
+}
+
+export interface IFilter {
+    page: number,
+    limit: number,
+    sorting?: string,
+    search?: '',
+    include?: string,
+    groupId?: string,
+    leaveTypeId?: string,
+    month?: string,
+    isAll?: boolean,
+    name?: string
+    // locationId?: string
+}
+
+@Injectable()
+export class HttpService {
+    constructor(private config: EnvironmentConfig,
+                private http: Http) {
+    }
+
+    createAuthorizationHeader(headers: Headers): Headers {
+        if (_.isUndefined(headers)) headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let token = localStorage.getItem('access_token');
+        if (token !== null) headers.append('Authorization', `${localStorage.getItem('token_type')} ${token}`);
+        headers.append('Accept-Language', localStorage.getItem('language') === 'zh-hk' ? 'en-us;q=0.1,zh-hk;q=0.2' : 'zh-hk;q=0.1,en-us;q=0.2');
+        return headers;
+    }
+
+    getCurrentTeamId(): string {
+        return localStorage.getItem('team');
+    }
+
+    get(url: string, options: RequestOptionsArgs = {params: new URLSearchParams()}): Observable<any> {
+        const _that = this;
+        options.headers = this.createAuthorizationHeader(options.headers);
+        if (options.params instanceof URLSearchParams) options.params.set('teamId', this.getCurrentTeamId());
+        return this.http
+            .get(this.config.apiEndPoint + url, options)
+            // .catch(this.handleError);
+            .catch((error) => {
+                const errorMsg = error.json().message || `Server error`;
+
+                // throw an application level error
+                // if (error.json().code === 11000 && error.json().message === 'authorization token is incorrect.') _that.router.navigate(['/auth/login']);
+                return Observable.throw(errorMsg);
+            });
+    }
+
+    getWithoutAppId(url: string, options: RequestOptionsArgs = {params: new URLSearchParams()}): Observable<any> {
+        const _that = this;
+        options.headers = this.createAuthorizationHeader(options.headers);
+        return this.http
+            .get(this.config.apiEndPoint + url, options)
+            // .catch(this.handleError);
+            .catch((error) => {
+                const errorMsg = error.json().message || `Server error`;
+
+                // throw an application level error
+                // if (error.json().code === 11000 && error.json().message === 'authorization token is incorrect.') _that.router.navigate(['/auth/login']);
+                return Observable.throw(errorMsg);
+            });
+    }
+
+    post(url: string, body: any = {}, options: RequestOptionsArgs = {}): Observable<any> {
+        const _that = this;
+        options.headers = this.createAuthorizationHeader(options.headers);
+        if (_.isUndefined(body.teamId)) body.teamId = this.getCurrentTeamId();
+        return this.http
+            .post(this.config.apiEndPoint + url, JSON.stringify(body), options)
+            // .catch(this.handleError);
+            .catch((error) => {
+                const errorMsg = error.json().message || `Server error`;
+
+                // throw an application level error
+                // if (error.json().code === 11000 && error.json().message === 'authorization token is incorrect.') _that.router.navigate(['/auth/login']);
+                return Observable.throw(errorMsg);
+            });
+    }
+
+    put(url: string, body: any = {}, options: RequestOptionsArgs = {}): Observable<any> {
+        const _that = this;
+        options.headers = this.createAuthorizationHeader(options.headers);
+        body.teamId = this.getCurrentTeamId();
+        return this.http
+            .put(this.config.apiEndPoint + url, JSON.stringify(body), options)
+            // .catch(this.handleError);
+            .catch((error) => {
+                const errorMsg = error.json().message || `Server error`;
+
+                // throw an application level error
+                // if (error.json().code === 11000 && error.json().message === 'authorization token is incorrect.') _that.router.navigate(['/auth/login']);
+                return Observable.throw(errorMsg);
+            });
+    }
+
+    delete(url: string, body: any = {}, options: RequestOptionsArgs = {}): Observable<any> {
+        const _that = this;
+        options.headers = this.createAuthorizationHeader(options.headers);
+        return this.http
+            .delete(this.config.apiEndPoint + url + '?teamId=' + this.getCurrentTeamId(), options)
+            // .catch(this.handleError);
+            .catch((error) => {
+                const errorMsg = error.json().message || `Server error`;
+
+                // throw an application level error
+                // if (error.json().code === 11000 && error.json().message === 'authorization token is incorrect.') _that.router.navigate(['/auth/login']);
+                return Observable.throw(errorMsg);
+            });
+    }
+
+    handleError(error: any): Observable<any> {
+        let errorMsg = error.json().message || `Server error`;
+
+        // throw an application level error
+        return Observable.throw(errorMsg);
+    }
+
+    /**
+     * Converts an object to a parametrised string.
+     * @param object
+     * @returns {string}
+     */
+    objectToParams(object: any): string {
+        //不编码
+        return Object.keys(object).map((key) => `${key}=${object[key]}`).join('&');
+
+        //编码
+        // return Object.keys(object).map(
+        //   (key) =>
+        //     _.isObject(object[key]) ?
+        //       this.subObjectToParams(encodeURIComponent(key), object[key]) :
+        //       `${encodeURIComponent(key)}=${encodeURIComponent(object[key])}`
+        // ).join('&');
+    }
+
+    /**
+     * Converts a sub-object to a parametrised string.
+     * @param object
+     * @returns {string}
+     */
+    private subObjectToParams(key: string, object: any): string {
+        return Object.keys(object).map((childKey) => _.isObject(object[childKey]) ?
+            this.subObjectToParams(`${key}[${encodeURIComponent(childKey)}]`, object[childKey]) :
+            `${key}[${encodeURIComponent(childKey)}]=${encodeURIComponent(object[childKey])}`
+        ).join('&');
+    }
+}
---
+++ src/providers/native.service.ts
@@ -0,0 +1,142 @@
+/**
+ * Created by yanxiaojun617@163.com on 12-27.
+ */
+import {Injectable} from "@angular/core";
+import {ToastController, LoadingController, Platform, Loading, AlertController} from "ionic-angular";
+import {StatusBar} from "@ionic-native/status-bar";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {Toast} from "@ionic-native/toast";
+import {AppMinimize} from "@ionic-native/app-minimize";
+import {Observable} from "rxjs";
+
+declare var LocationPlugin;
+declare var AMapNavigation;
+
+@Injectable()
+export class NativeService {
+    private loading: Loading;
+
+    constructor(private platform: Platform,
+                private toastCtrl: ToastController,
+                private alertCtrl: AlertController,
+                private statusBar: StatusBar,
+                private splashScreen: SplashScreen,
+                private toast: Toast,
+                private appMinimize: AppMinimize,
+                private loadingCtrl: LoadingController) {
+    }
+
+    /**
+     * 状态栏
+     */
+    statusBarStyle(statusColor:string): void {
+        if (this.isMobile()) {
+            this.statusBar.overlaysWebView(false);
+            this.statusBar.styleLightContent();
+            this.statusBar.backgroundColorByHexString(statusColor);//3261b3
+        }
+    }
+
+    /**
+     * 隐藏启动页面
+     */
+    splashScreenHide(): void {
+        this.isMobile() && this.splashScreen.hide();
+    }
+
+    /**
+     * 调用最小化app插件
+     */
+    minimize(): void {
+        this.appMinimize.minimize()
+    }
+
+    /**
+     * 是否真机环境
+     */
+    isMobile(): boolean {
+        return this.platform.is('mobile') && !this.platform.is('mobileweb');
+    }
+
+    /**
+     * 是否android真机环境
+     */
+    isAndroid(): boolean {
+        return this.isMobile() && this.platform.is('android');
+    }
+
+    /**
+     * 是否ios真机环境
+     */
+    isIos(): boolean {
+        return this.isMobile() && (this.platform.is('ios') || this.platform.is('ipad') || this.platform.is('iphone'));
+    }
+
+    /**
+     * 只有一个确定按钮的弹出框.
+     * 如果已经打开则不再打开
+     */
+    alert = (() => {
+        let isExist = false;
+        return (title: string, subTitle: string = '', message: string = '', callBackFun = null): void => {
+            if (!isExist) {
+                isExist = true;
+                this.alertCtrl.create({
+                    title: title,
+                    subTitle: subTitle,
+                    message: message,
+                    buttons: [{
+                        text: '确定', handler: () => {
+                            isExist = false;
+                            callBackFun && callBackFun();
+                        }
+                    }],
+                    enableBackdropDismiss: false
+                }).present();
+            }
+        };
+    })();
+
+    /**
+     * 统一调用此方法显示提示信息
+     * @param message 信息内容
+     * @param duration 显示时长
+     */
+    showToast(message: string = '操作完成', duration: number = 2000): void {
+        if (this.isMobile()) {
+            this.toast.show(message, String(duration), 'center').subscribe();
+        } else {
+            this.toastCtrl.create({
+                message: message,
+                duration: duration,
+                position: 'middle',
+                showCloseButton: false
+            }).present();
+        }
+    };
+
+    /**
+     * 统一调用此方法显示loading
+     * @param content 显示的内容
+     */
+    showLoading(content: string = ''): void {
+        if(!this.loading){//如果loading已经存在则不再打开
+            let loading = this.loadingCtrl.create({
+                content: content
+            });
+            loading.present();
+            this.loading = loading;
+        }
+    };
+
+    /**
+     * 关闭loading
+     */
+    hideLoading(): void {
+        setTimeout(() => {//延迟200毫秒可以避免嵌套请求loading重复打开和关闭
+            this.loading && this.loading.dismiss();
+            this.loading = null;
+        }, 200);
+    };
+
+}
